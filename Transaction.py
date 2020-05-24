29c3e76b772a2a23aeed31f34da5eeaa401c7f18
Basem Rizk <basem.rizk@outlook.com>
2020-05-25 00:04:51 +0200

    creating transaction object iff coins amount is available

--- Transaction.py
+++ Transaction.py
@@ -2,6 +2,15 @@ class Transaction:
 
     _current_id = 0
         
+    
+    def __new__(cls, sender, amount, recipient_vk): 
+        print("Creating Transaction") 
+        # Create transaction only if coins available 
+        if len(sender.get_coins(amount)) == amount:            
+            return super(Transaction, cls).__new__(cls) 
+        else:
+            return None
+     
     def __init__(self, sender, amount, recipient_vk):
         self.id = _current_id
         self.sender = sender
@@ -10,13 +19,11 @@ class Transaction:
         self.prev_hash_pt = None
         self.hash = None
         self.signature = None
-        # TODO
+        # Already verified
         self.coins = sender.get_coins(amount)
-
         _current_id += 1
 
     def __str__(self):
         return str(self.id) + self.prev_hash_pt + str(self.amount) + self.recipient
     
     
-    # TODO Create coins after get_coins 
