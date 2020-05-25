5a719819d05fa0b42c2e0bb8c465115b7bc24f9f
Basem Rizk <basem.rizk@outlook.com>
2020-05-25 18:25:14 +0200

    fix undefined vars

--- Block.py
+++ Block.py
@@ -7,16 +7,16 @@ class Block:
     _capacity = 10
     
     def __init__(self, prev_hash_pt):
-        self.id = _current_id
+        self.id = self._current_id
         self.prev_hash_pt = prev_hash_pt
         self.transactions = []
         self.hash = None
 
-        _current_id += 1
+        self._current_id += 1
     
     def add_transaction(self, trans):
         self._transactions.append(trans)
-        if len(self.transactions) >= _capacity:
+        if len(self.transactions) >= self._capacity:
             return True
         return False
 
